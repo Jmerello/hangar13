46167d30b18aaabc072ff96abdd2da540c7f9fd7
Gearheaddesigns <88631092+Jmerello@users.noreply.github.com>
2026-07-23 04:53:13 -0500

    Update button.js

--- core/components/button/button.js
+++ core/components/button/button.js
@@ -1,1 +1,26 @@
 
+class H13Button {
+
+    static loading(button){
+
+        button.classList.add("loading");
+
+        button.disabled = true;
+
+        button.dataset.original = button.innerHTML;
+
+        button.innerHTML = "Loading...";
+
+    }
+
+    static finished(button){
+
+        button.classList.remove("loading");
+
+        button.disabled = false;
+
+        button.innerHTML = button.dataset.original;
+
+    }
+
+}
