ff6fe6cdb45c94c08f8ba23c749716a8da6d3d8a
Gearheaddesigns <88631092+Jmerello@users.noreply.github.com>
2026-07-23 05:13:00 -0500

    Update card.js

--- core/components/card/card.js
+++ core/components/card/card.js
@@ -1,1 +1,45 @@
 
+class H13Card{
+
+    constructor(element){
+
+        this.card = element;
+
+    }
+
+    collapse(){
+
+        const body =
+            this.card.querySelector(".h13-card-body");
+
+        body.style.display="none";
+
+    }
+
+    expand(){
+
+        const body =
+            this.card.querySelector(".h13-card-body");
+
+        body.style.display="block";
+
+    }
+
+    toggle(){
+
+        const body =
+            this.card.querySelector(".h13-card-body");
+
+        if(body.style.display==="none"){
+
+            body.style.display="block";
+
+        }else{
+
+            body.style.display="none";
+
+        }
+
+    }
+
+}
